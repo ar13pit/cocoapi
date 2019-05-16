c29247cfa8ff2f8250064ca862551a4fbb7ff641
Arpit Aggarwal <ar13pit@gmail.com>
2019-05-16 17:43:27 +0200

    support install using pip

--- PythonAPI/setup.py
+++ PythonAPI/setup.py
@@ -1,14 +1,25 @@
+import os
 from setuptools import setup, Extension
 import numpy as np
 
 # To compile and install locally run "python setup.py build_ext --inplace"
 # To install library to Python site-packages run "python setup.py build_ext install"
+common_dir = None
+
+if os.path.exists("common"):
+    if not os.path.islink("common"):
+        common_dir = "common"
+
+if not common_dir:
+    common_dir = "../common"
+
+maskApi_path = os.path.realpath(os.path.join(common_dir, "maskApi.c"))
 
 ext_modules = [
     Extension(
         'pycocotools._mask',
-        sources=['../common/maskApi.c', 'pycocotools/_mask.pyx'],
-        include_dirs = [np.get_include(), '../common'],
+        sources=[maskApi_path, 'pycocotools/_mask.pyx'],
+        include_dirs = [np.get_include(), common_dir],
         extra_compile_args=['-Wno-cpp', '-Wno-unused-function', '-std=c99'],
     )
 ]
